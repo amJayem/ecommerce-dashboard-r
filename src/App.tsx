322cd15e31de9ac3147c8ab19f8509bfec2bcbbc
amjayem <csejayem19@gmail.com>
2025-11-23 00:26:06 +0600

    feat: add Web Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { RouterProvider } from "react-router-dom"
-import { router } from "@/routes"
-import { AuthProvider } from "./contexts/AuthContext"
+import { RouterProvider } from 'react-router-dom'
+import { router } from '@/routes'
+import { AuthProvider } from './contexts/AuthContext'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
     <AuthProvider>
+      <Analytics />
       <RouterProvider router={router} />
     </AuthProvider>
   )
